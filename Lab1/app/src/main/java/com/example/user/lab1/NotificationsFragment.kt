e50cd618da02a6e425787970f4ce53c0d28996b0
dcalance <daniel.calancea@faf.utm.md>
2017-10-18 00:17:04 +0300

    Added content to search fragment and notification fragment. Laboratory finished

--- Lab1/app/src/main/java/com/example/user/lab1/NotificationsFragment.kt
+++ Lab1/app/src/main/java/com/example/user/lab1/NotificationsFragment.kt
@@ -1,13 +1,20 @@
 package com.example.user.testapp
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.NotificationCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.user.lab1.R
+import android.content.Intent
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 
 /**
@@ -21,5 +28,35 @@ import com.example.user.lab1.R
 class NotificationsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_notifications, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        notificationBtn.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                val mBuilder: NotificationCompat.Builder = NotificationCompat.Builder(activity)
+                mBuilder.setSmallIcon(R.drawable.notification_bg_low)
+                mBuilder.setContentTitle("Lab1 Notification")
+                mBuilder.setContentText("This notification will close automatically in 10 seconds")
+
+                val resultIntent = Intent(activity, MainActivity::class.java)
+                val resultPendingIntent = PendingIntent.getActivity(activity, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+                mBuilder.setContentIntent(resultPendingIntent)
+                val mNotificationId = 1
+                val mNotifyMgr = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                mNotifyMgr.notify(mNotificationId, mBuilder.build())
+
+                object : CountDownTimer(10000, 1000) {
+                    override fun onFinish() {
+                        mNotifyMgr.cancel(mNotificationId)
+                    }
+                    override fun onTick(millisUntilFinished: Long) {
+                    }
+                }.start()
+            }
+        })
+
     }
 }
--- Lab1/app/src/main/java/com/example/user/lab1/SearchFragment.kt
+++ Lab1/app/src/main/java/com/example/user/lab1/SearchFragment.kt
@@ -1,6 +1,8 @@
 package com.example.user.testapp
 
+import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -8,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.user.lab1.R
+import kotlinx.android.synthetic.main.fragment_search.*
 
 
 /**
@@ -21,5 +24,18 @@ import com.example.user.lab1.R
 class SearchFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        searchBtn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val searchString = searchField.text.toString()
+                val browserIntent = Intent(Intent.ACTION_WEB_SEARCH)
+                browserIntent.putExtra(SearchManager.QUERY, searchString)
+                startActivity(browserIntent)
+            }
+        })
     }
 }
