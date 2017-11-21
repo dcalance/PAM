09621b9ad1a20a1f3ca0dffe5e7e5e21dad1fd2b
dcalance <daniel.calancea@faf.utm.md>
2017-11-21 21:50:48 +0200

    Finished Lab3

--- Lab3/app/src/main/java/com/example/user/lab3/SmsFragment.kt
+++ Lab3/app/src/main/java/com/example/user/lab3/SmsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.user.lab3
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.InputFilter
@@ -16,21 +17,16 @@ import android.webkit.WebResourceRequest
 import android.text.Editable
 import android.text.TextWatcher
 import android.graphics.BitmapFactory
+import android.net.ConnectivityManager
 import android.os.AsyncTask
 import android.util.Log
 import android.webkit.WebResourceError
 import android.widget.ImageView
+import android.widget.Toast
 import com.example.user.lab3.R.id.webView
 import com.example.user.lab3.R.id.webView
 
 
-
-
-
-
-/**
- * A simple [Fragment] subclass.
- */
 class SmsFragment : Fragment() {
 
 
@@ -40,7 +36,6 @@ class SmsFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        webView.addJavascriptInterface(WebAppInterface(activity, charLeft, captchaImage, topImage), "Android")
 
         webView.webViewClient = object : WebViewClient() {
 
@@ -49,8 +44,15 @@ class SmsFragment : Fragment() {
             }
 
             override fun onPageFinished(view: WebView?, url: String?) {
-                    webView.loadUrl("javascript: (function() {Android.setCharsLeft(document.getElementById('rest').value);}) ();")
-                    webView.loadUrl("javascript: (function() {Android.setCaptchaImage(document.getElementsByClassName(\"col-lg-8 col-md-9 col-xs-12\")[0].getElementsByTagName('img')[0].src);}) ();")
+                val printMessageBox = "javascript: (function() {" +
+                        "var div = document.getElementsByClassName(\"col-xs-12 col-sm-8\")[0];" +
+                        "div.style.position = 'absolute';" +
+                        "div.style.top = '20px';" +
+                        "div.style.left = '0px';" +
+                        "var divHtml = document.getElementsByClassName(\"col-xs-12 col-sm-8\")[0].outerHTML;" +
+                        "document.body.innerHTML = divHtml;" +
+                        "}) ();"
+                webView.loadUrl(printMessageBox)
 
             }
 
@@ -59,65 +61,26 @@ class SmsFragment : Fragment() {
                 Log.d("Error webview", error.toString())
             }
         }
-        webView.loadUrl("https://orangetext.md/")
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
-        //webSettings.domStorageEnabled = true
+        loadPage("https://orangetext.md/")
+        refreshBtn.setOnClickListener { _ ->
+            loadPage("https://orangetext.md/")
+        }
 
-        charLeft.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable) {
-                // TODO Auto-generated method stub
-            }
+    }
 
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-                // TODO Auto-generated method stub
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                messageText.filters = arrayOf(InputFilter.LengthFilter(charLeft.text.toString().toInt()))
-            }
-        })
-
-        messageText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if (p2 < p3) {
-                    val left = charLeft.text.toString().toInt() - 1
-                    charLeft.text = left.toString()
-                } else {
-                    val left = charLeft.text.toString().toInt() + 1
-                    charLeft.text = left.toString()
-                }
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-        })
-
-        sentBtn.setOnClickListener { _ ->
-            webView.loadUrl("javascript: (function() {Android.setCaptchaImage(document.getElementsByClassName(\"col-lg-8 col-md-9 col-xs-12\")[0].getElementsByTagName('img')[0].src);}) ();")
-            val setFrom = "document.getElementById('From').value= '${fromText.text}';"
-            val setTo = "document.getElementById('Msisdn').value= '${toText.text}';"
-            val setMessage = "document.getElementById('Message').value= '${messageText.text}';"
-            val setCaptcha = "document.getElementById('Captcha').value= '${captchaText.text}';"
-            //val clickButton = "document.getElementById('btnOK').click();"
-            //val printError = "Android.showToast(document.getElementById('ws_errors').innerHTML);"
-            webView.visibility = View.VISIBLE
-            //captchaImage.visibility = View.GONE
-
-            //webView.loadUrl("javascript: (function() { $setFrom $setTo $setMessage $setCaptcha }) ();")
-
-//            webView.loadUrl("javascript: (function() {document.getElementById('From').value= '${fromText.text}';}) ();")
-//            webView.loadUrl("javascript: (function() {document.getElementById('Msisdn').value= '${toText.text}';}) ();")
-//            webView.loadUrl("javascript: (function() {document.getElementById('Message').value= '${messageText.text}';}) ();")
-//            webView.loadUrl("javascript: (function() {document.getElementById('Captcha').value= '${captchaText.text}';}) ();")
-//            webView.loadUrl("javascript: (function() {document.getElementById('btnOK').click();}) ();")
-//            webView.loadUrl("javascript: (function() {Android.showToast(document.getElementsByClass('ws_errors')[0].textContent);}) ();")
+    private fun loadPage(url : String) {
+        if (isNetworkAvailable()) {
+            webView.loadUrl(url)
+            val webSettings = webView.settings
+            webSettings.javaScriptEnabled = true
+        } else {
+            Toast.makeText(context, "No Internet Connection", Toast.LENGTH_LONG).show()
         }
     }
 
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
 }
